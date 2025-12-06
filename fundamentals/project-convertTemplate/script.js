acb5ddefebad3b0f8f82b3c96b636aa25eba5085
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-12-06 08:16:09 -0300

    feat: implement currency conversion functionality with form handling

---
+++ fundamentals/project-convertTemplate/script.js
@@ -0,0 +1,34 @@
+// Cotação de moedas do dia.
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
+
+// Captando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
+};
