a695280ba7a4cd413e21099fac516170b1f483ac
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-12-07 10:02:12 -0300

    feat: implement expense item creation and display in the expense list

--- fundamentals/project-refund/script.js
+++ fundamentals/project-refund/script.js
@@ -3,6 +3,8 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
+const expenseList = document.querySelector("ul");
+
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
 
@@ -34,7 +36,15 @@ form.onsubmit = (event) => {
 
 function expenseAdd(newExpense) {
   try {
-    throw new Error("Erro de teste");
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    expenseItem.append(expenseIcon);
+    expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
