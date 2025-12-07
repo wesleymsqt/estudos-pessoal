fe81a1253d00efd0b4bebb7414d5ff78e66de716
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-12-07 09:50:15 -0300

    feat: add form handling and input elements for refund request

--- fundamentals/project-refund/script.js
+++ fundamentals/project-refund/script.js
@@ -1,4 +1,7 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -13,3 +16,7 @@ function formatCurrencyBRL(value) {
     currency: "BRL",
   });
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+};
