e59189b652c8bf429ea7f9540a52784c49c65d27
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-12-08 07:41:50 -0300

    feat: add total expenses count update functionality

--- fundamentals/project-refund/script.js
+++ fundamentals/project-refund/script.js
@@ -4,6 +4,7 @@ const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
 const expenseList = document.querySelector("ul");
+const expensesQuantity = document.querySelector("aside header p span");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -65,8 +66,23 @@ function expenseAdd(newExpense) {
     expenseInfo.append(expenseName, expenseCategory);
     expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
     expenseList.append(expenseItem);
+
+    updateTotals();
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
+  }
+}
+
+function updateTotals() {
+  try {
+    const items = expenseList.children;
+
+    expensesQuantity.textContent = `${items.length} ${
+      items.length > 1 ? "despesas" : "despesa"
+    }`;
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível atualizar os totais.");
   }
 }
