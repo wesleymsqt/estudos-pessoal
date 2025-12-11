72d6086a8caa1d728030c823c86ea927e34f5820
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-12-11 13:01:58 -0300

    feat: add password visibility toggle functionality and styles

--- fundamentals/project-loginScreen/app.js
+++ fundamentals/project-loginScreen/app.js
@@ -1,6 +1,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const togglePasswordButtons = document.querySelectorAll(".toggle-password");
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -8,4 +9,20 @@ sign_up_btn.addEventListener("click", () => {
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+});
+
+togglePasswordButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const input = btn.previousElementSibling;
+
+    if (input.type === "password") {
+      input.type = "text";
+      btn.classList.remove("fa-eye-slash");
+      btn.classList.add("fa-eye");
+    } else {
+      input.type = "password";
+      btn.classList.remove("fa-eye");
+      btn.classList.add("fa-eye-slash");
+    }
+  });
 });
