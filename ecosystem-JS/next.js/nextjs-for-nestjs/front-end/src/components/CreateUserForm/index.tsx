e0099d88cd23d40fe4b126430db5e9f25ea0ed33
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2026-06-02 09:02:34 -0300

    refactor: clean up CreateUserForm component by removing unused code and simplifying imports

--- ecosystem-JS/next.js/nextjs-for-nestjs/front-end/src/components/CreateUserForm/index.tsx
+++ ecosystem-JS/next.js/nextjs-for-nestjs/front-end/src/components/CreateUserForm/index.tsx
@@ -1,84 +1,21 @@
-'use client';
+"use client";
 
-import { InputText } from '@/components/InputText';
-import clsx from 'clsx';
-import { UserRoundIcon } from 'lucide-react';
-import Link from 'next/link';
-import { Button } from '../Button';
-import { PublicUserSchema } from '@/lib/user/schemas';
-import { useActionState, useEffect } from 'react';
-import { toast } from 'react-toastify';
-import { createUserAction } from '@/actions/user/create-user-action';
-import { HoneypotInput } from '../HoneypotInput';
+import { InputText } from "@/components/InputText";
+import clsx from "clsx";
+import { UserRoundIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../Button";
 
 export function CreateUserForm() {
-  const [state, action, isPending] = useActionState(createUserAction, {
-    user: PublicUserSchema.parse({}),
-    errors: [],
-    success: false,
-  });
-
-  useEffect(() => {
-    toast.dismiss();
-    if (state.errors.length > 0) {
-      state.errors.forEach(error => toast.error(error));
-    }
-  }, [state]);
-
   return (
     <div
       className={clsx(
-        'flex items-center justify-center',
-        'text-center max-w-sm mt-16 mb-32 mx-auto',
+        "flex items-center justify-center",
+        "text-center max-w-sm mt-16 mb-32 mx-auto",
       )}
     >
-      <form action={action} className='flex-1 flex flex-col gap-6'>
-        <InputText
-          type='text'
-          name='name'
-          labelText='Nome'
-          placeholder='Seu nome'
-          disabled={isPending}
-          defaultValue={state.user.name}
-          required
-        />
-        <InputText
-          type='email'
-          name='email'
-          labelText='E-mail'
-          placeholder='Sua e-mail'
-          disabled={isPending}
-          defaultValue={state.user.email}
-          required
-        />
-        <InputText
-          type='password'
-          name='password'
-          labelText='Senha'
-          placeholder='Sua senha'
-          disabled={isPending}
-          required
-        />
-        <InputText
-          type='password'
-          name='password2'
-          labelText='Repetir senha'
-          placeholder='Sua senha novamente'
-          disabled={isPending}
-          required
-        />
-
-        <HoneypotInput />
-
-        <Button disabled={isPending} type='submit' className='mt-4'>
-          <UserRoundIcon />
-          {!isPending && 'Criar conta'}
-          {isPending && 'Criando...'}
-        </Button>
-
-        <p className='text-sm/tight'>
-          <Link href='/login'>Já tem conta? Entrar</Link>
-        </p>
+      <form action={""} className="flex-1 flex flex-col gap-6">
+        
       </form>
     </div>
   );
