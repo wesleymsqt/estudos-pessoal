62aa8b0851b996d07a8307f0a73daf2790b39359
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2026-06-03 09:37:12 -0300

    feat: implement user creation form with action handling and error notifications

--- ecosystem-JS/next.js/nextjs-for-nestjs/front-end/src/components/CreateUserForm/index.tsx
+++ ecosystem-JS/next.js/nextjs-for-nestjs/front-end/src/components/CreateUserForm/index.tsx
@@ -5,8 +5,25 @@ import clsx from "clsx";
 import { UserRoundIcon } from "lucide-react";
 import Link from "next/link";
 import { Button } from "../Button";
+import { PublicUserSchema } from "@/lib/user/schemas";
+import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { createUserAction } from "@/actions/user/create-user-action";
 
 export function CreateUserForm() {
+  const [state, action, isPending] = useActionState(createUserAction, {
+    user: PublicUserSchema.parse({}),
+    errors: [],
+    success: false,
+  });
+
+  useEffect(() => {
+    toast.dismiss();
+    if (state.errors.length > 0) {
+      state.errors.forEach((error) => toast.error(error));
+    }
+  }, [state]);
+
   return (
     <div
       className={clsx(
@@ -14,14 +31,14 @@ export function CreateUserForm() {
         "text-center max-w-sm mt-16 mb-32 mx-auto",
       )}
     >
-      <form action={""} className="flex-1 flex flex-col gap-6">
+      <form action={action} className="flex-1 flex flex-col gap-6">
         <InputText
           type="text"
           name="name"
           labelText="Nome"
           placeholder="Seu nome"
-          disabled={false}
-          defaultValue={""}
+          disabled={isPending}
+          defaultValue={state.user.name}
           required
         />
         <InputText
@@ -29,8 +46,8 @@ export function CreateUserForm() {
           name="email"
           labelText="E-mail"
           placeholder="Sua e-mail"
-          disabled={false}
-          defaultValue={""}
+          disabled={isPending}
+          defaultValue={state.user.email}
           required
         />
         <InputText
@@ -38,7 +55,7 @@ export function CreateUserForm() {
           name="password"
           labelText="Senha"
           placeholder="Sua senha"
-          disabled={false}
+          disabled={isPending}
           required
         />
         <InputText
@@ -46,13 +63,14 @@ export function CreateUserForm() {
           name="password2"
           labelText="Repetir senha"
           placeholder="Sua senha novamente"
-          disabled={false}
+          disabled={isPending}
           required
         />
 
-        <Button disabled={false} type="submit" className="mt-4">
+        <Button disabled={isPending} type="submit" className="mt-4">
           <UserRoundIcon />
-          Criar conta
+          {!isPending && "Criar conta"}
+          {isPending && "Criando..."}
         </Button>
 
         <p className="text-sm/tight">
